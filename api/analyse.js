eec0bd934d9b838560fdcbc78949bd9051f92199
daftchip <daft.1981@hotmail.co.uk>
2026-05-18 10:06:05 +0100

    Update analyse.js

--- api/analyse.js
+++ api/analyse.js
@@ -1,3 +1,11 @@
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '20mb',
+    },
+  },
+};
+
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
